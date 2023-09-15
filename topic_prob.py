ba795858b49c3826cac017c9837ddc099661b13e
nimish-d <nimish.d@ihx.in>
2023-09-15 03:43:40 +0000

    added copyrights to topic_prob.py

--- topic_prob.py
+++ topic_prob.py
@@ -1,3 +1,12 @@
+#####################################################################
+# Copyright(C), 2023 IHX Private Limited. All Rights Reserved
+# Unauthorized copying of this file, via any medium is 
+# strictly prohibited 
+#
+# Proprietary and confidential
+# email: care@ihx.in
+#####################################################################
+
 import copy
 from itertools import combinations, product
 import numpy as np
